a837ed6e06d84a1e98019818516acccdf3abe309
Qeggs <get_boot@outlook.com>
2025-12-09 17:04:06 +0800

    feat(core): 添加延迟任务池功能
    新增 `DelayedTasksPool` 类，用于管理异步延迟任务。支持添加带延时的协程任务、
    等待所有任务完成、取消所有任务以及关闭任务池。通过 `asyncio` 实现任务调度，
    并使用 `loguru` 记录任务执行日志。

---
+++ core/Delayed_Tasks_Pool/_pool.py
@@ -0,0 +1,62 @@
+import asyncio
+from typing import TypeVar
+from collections.abc import Coroutine
+from loguru import logger
+
+T = TypeVar('T')
+
+class DelayedTasksPool:
+    def __init__(self):
+        self.tasks: set[asyncio.Task] = set()
+    
+    async def _task_warper(self, sleep_time: float, task: Coroutine[None, None, T], id: str | None = None) -> T:
+        """Wrapper for tasks to add them to the pool"""
+        if id is None:
+            id = task.__name__
+        logger.trace(
+            "Adding task to pool: {id}",
+            id = id
+        )
+        try:
+            logger.trace(
+                "Task {id} will be executed after {sleep_time} seconds",
+                id = id,
+                sleep_time = sleep_time
+            )
+            await asyncio.sleep(sleep_time)
+        finally:
+            logger.trace(
+                "Task {id} is being executed",
+                id = id
+            )
+            result = await task
+        
+        return result
+
+    async def add_task(self, sleep_time: float, task: Coroutine[None, None, T], id: str | None = None):
+        """Add task to the pool"""
+        self.tasks.add(
+            asyncio.create_task(
+                self._task_warper(
+                    sleep_time = sleep_time,
+                    task = task
+                )
+            )
+        )
+
+    async def wait_all(self):
+        """Wait for all tasks in the pool to complete"""
+        await asyncio.gather(*self.tasks)
+        self.tasks.clear()
+    
+    async def cancel_all(self, wait: bool = True):
+        """Cancel all tasks in the pool"""
+        for task in self.tasks:
+            task.cancel()
+            if wait:
+                await task
+        self.tasks.clear()
+    
+    async def close(self):
+        """Close the pool"""
+        await self.cancel_all()
